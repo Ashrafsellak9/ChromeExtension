3f64a08e60f024630677f6386ea0b7cfb70dd10a
Ashrafsellak9 <sellakachraf9@gmail.com>
2026-09-06 17:08:38 +0100

    add logic for saveInput

---
+++ index.js
@@ -0,0 +1,9 @@
+let myLeads = [];
+let inputEl = document.getElementById("input-el");
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", () => {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    console.log(myLeads);
+});
