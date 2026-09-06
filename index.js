c37c590e8fed072682510b9b0e20b281207a9835
Ashrafsellak9 <sellakachraf9@gmail.com>
2026-09-06 18:59:28 +0100

    Update logic (innerHTML)

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let myLeads = [];
 let inputEl = document.getElementById("input-el");
 let inputBtn = document.getElementById("input-btn");
+let ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", () => {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    console.log(myLeads);
+      myLeads.push(inputEl.value);
+      console.log(myLeads);  
 });
+
+for(let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += `<li>${myLeads[i]}</li>`;
+}
