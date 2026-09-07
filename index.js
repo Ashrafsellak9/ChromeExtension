dcc047ef32ef8ebaae565bda8a5a142439d309e2
Ashrafsellak9 <sellakachraf9@gmail.com>
2026-09-07 17:56:54 +0100

    add renderLeads

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-let myLeads = [];
 let inputEl = document.getElementById("input-el");
 let inputBtn = document.getElementById("input-btn");
 let ulEl = document.getElementById("ul-el");
 
+let myLeads = []
+
 inputBtn.addEventListener("click", () => {
-      myLeads.push(inputEl.value);
-      renderLeads() 
-});
+    myLeads.push(inputEl.value);
+    renderLeads();
+    inputEl.value = "";
+})
 
 const renderLeads = () => {
     let listItems = "";
     for(let i = 0; i < myLeads.length; i++) {
-    listItems += `<li>${myLeads[i]}</li>`;
-}
+        listItems += `<li>${myLeads[i]}</li>`;
+    }
     ulEl.innerHTML = listItems;
 }
